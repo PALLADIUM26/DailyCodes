8756edfa3c122ce96395ad1a439ef531edbc1c86
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-11-09 01:28:50 +0530

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,1 +1,9 @@
+n = int(input())
 
+for i in range(0, n):
+	a, b, c = [int(x) for x in input().split()]
+	# print(type(a))
+	if ((a+b >= c) and (b+c >= a) and (c+a >= b)):
+		print(a+b+c)
+	else:
+		print(-1)
