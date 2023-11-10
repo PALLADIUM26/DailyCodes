1bea4602cff1815bb0dcbef10048320d134427ee
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-11-10 21:40:07 +0530

    Update Day2B.java

--- Day2B.java
+++ Day2B.java
@@ -1,1 +1,22 @@
+// Pascals's Triangle
+// link: https://leetcode.com/problems/pascals-triangle/
+// change classname Day2B to Solution at portal
 
+class Day2B {
+    public List<List<Integer>> generate(int rowNo) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+		    int i, element;
+
+    		for(int j=1; j<=rowNo; j++){
+            List<Integer> list2 = new ArrayList<Integer>();
+            list2.add(1);
+            for(i=1; i<j; i++){
+                element = list2.get(i-1)*(j-i)/i;
+                list2.add(element);
+            }
+            list.add(list2);
+		    }
+
+        return list;
+    }
+}
